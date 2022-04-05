6f476fb594ada4d84634bd1739c7a1b6a4db6ba8
cleave13 <94774869+cleave13@users.noreply.github.com>
2022-04-04 22:00:38 -0600

    removed unnecessary variable declarations from seed file

--- seeds/seed.js
+++ seeds/seed.js
@@ -12,19 +12,19 @@ const userData = require('./userData.json');
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  const user = await User.bulkCreate(userData, { returning: true });
+  await User.bulkCreate(userData, { returning: true });
   
-  const tournament = await Tournament.bulkCreate(tournamentData, { returning: true });
+  await Tournament.bulkCreate(tournamentData, { returning: true });
 
-  const round = await Round.bulkCreate(roundData, { returning: true });
+  await Round.bulkCreate(roundData, { returning: true });
 
-  const match = await Match.bulkCreate(matchData, { returning: true });
+  await Match.bulkCreate(matchData, { returning: true });
 
-  const team = await Team.bulkCreate(teamData, { returning: true });
+  await Team.bulkCreate(teamData, { returning: true });
 
-  const player = await Player.bulkCreate(playerData, { returning: true });
+  await Player.bulkCreate(playerData, { returning: true });
 
-  const score = await Score.bulkCreate(scoreData, { returning: true });
+  await Score.bulkCreate(scoreData, { returning: true });
 
   process.exit(0);
 };
