fc256081c6127b1618367a419f4f3f63052cd4f9
cleave13 <94774869+cleave13@users.noreply.github.com>
2022-04-06 08:52:13 -0600

    completed initial pass at all routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,22 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
 
+// require all route files and assign to variables
+const matchRoutes = require('./matchRoutes');
+const playerRoutes = require('./playerRoutes');
+const roundRoutes = require('./roundRoutes');
+const scoreRoutes = require('./scoreRoutes');
+const teamRoutes = require('./teamRoutes');
+const tournamentRoutes = require('./tournamentRoutes');
+const userRoutes = require('./userRoutes');
+
+// relate all api paths to route file variables
+router.use('/matches', matchRoutes);
+router.use('/players', playerRoutes);
+router.use('/rounds', roundRoutes);
+router.use('/scores', scoreRoutes);
+router.use('/teams', teamRoutes);
+router.use('/tournaments', tournamentRoutes);
 router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
+
 
 module.exports = router;
---
+++ controllers/api/matchRoutes.js
@@ -0,0 +1,73 @@
+const router = require('express').Router();
+const { Match } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+// api/matchs
+
+router.get('/', withAuth, async (req, res) => {
+    // find all matchs
+    try {
+        const matchData = await Match.findAll({});
+        res.status(200).json(matchData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// get one match
+router.get('/:id', withAuth, async (req, res) => {
+    // find a single match by its `id`
+    try {
+        const matchData = await Match.findByPk(req.params.id);
+        res.status(200).json(matchData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// create new match
+router.post('/', async (req, res) => {
+    try {
+        const matchData = await Match.create(req.body);
+        res.status(200).json(matchData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+// update match
+router.put('/:id', withAuth, (req, res) => {
+    // update match data
+    try {
+        const matchData = await Match.update(req.body, {
+            where: {
+                id: req.params.id,
+            },
+        });
+        res.status(200).json(matchData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+    // delete one match by its `id` value
+    try {
+        const matchData = await Match.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+
+        if (!matchData) {
+            res.status(404).json({ message: 'No match found with this id!' });
+            return;
+        }
+
+        res.status(200).json(matchData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
