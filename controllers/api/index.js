15934f386355c917d8c8d08890c5fdbd90f59966
cleave13 <94774869+cleave13@users.noreply.github.com>
2022-04-11 19:13:32 -0600

    removed extraneous files

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 
 // require all route files and assign to variables
 const matchRoutes = require('./matchRoutes');
-const playerRoutes = require('./playerRoutes');
 const roundRoutes = require('./roundRoutes');
 const scoreRoutes = require('./scoreRoutes');
 const teamRoutes = require('./teamRoutes');
@@ -11,7 +10,6 @@ const userRoutes = require('./userRoutes');
 
 // relate all api paths to route file variables
 router.use('/matches', matchRoutes);
-router.use('/players', playerRoutes);
 router.use('/rounds', roundRoutes);
 router.use('/scores', scoreRoutes);
 router.use('/teams', teamRoutes);
--- models/index.js
+++ models/index.js
@@ -1,6 +1,5 @@
 const Match = require('./Match');
 const Score = require('./Score');
-const Player = require('./Player');
 const Round = require('./Round');
 const Team = require('./Team');
 const Tournament = require('./Tournament');
@@ -33,18 +32,10 @@ Match.belongsTo(Round, {
   foreignKey: 'round_id'
 });
 
-Team.hasMany(Player, {
-  foreignKey: 'team_id'
-});
-
-Player.belongsTo(Team, {
-  foreignKey: 'team_id'
-});
-
 Match.belongsToMany(Team, { through: Score });
 
 Team.belongsToMany(Match, { through: Score });
 
 
 
-module.exports = { Match, Score, Player, Round, Team, Tournament, User };
+module.exports = { Match, Score, Round, Team, Tournament, User };
