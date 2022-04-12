2045f7eaee003ebe067df0cb7988891b3c5e646b
cleave13 <94774869+cleave13@users.noreply.github.com>
2022-04-12 06:40:31 -0600

    required User model in home routes and changed main page title

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Tournament, Round, Match, Score, Team } = require('../models');
+const { Tournament, Round, Match, Score, User, Team } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
